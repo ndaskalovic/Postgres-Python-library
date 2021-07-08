85f44099ea7da959e7130c9b441aa4596dbc4af1
ndaskalovic <55789531+ndaskalovic@users.noreply.github.com>
2021-07-08 16:46:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='pysqlpostgres',
-    version='0.0.1',
+    version='0.0.2',
     description='A Postgres database SQL query library',
     long_description='',
     py_modules=[''],
